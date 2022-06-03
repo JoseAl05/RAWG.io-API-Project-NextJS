7f62608d76878067f7bdf3f59bec11e910db72dd
JoseAl05 <jparancibialinker@gmail.com>
2022-06-02 20:25:26 -0400

    Added footer (Not Final)

--- pages/index.js
+++ pages/index.js
@@ -13,34 +13,61 @@ export default function Home() {
       <div>
         <div className={styles.disclaimer} role="disclaimer">
           <small>
-            <FontAwesomeIcon icon={faExclamationCircle} className={styles.exclamation} fixedWidth/>
-            The content of this website is completely free of charge. The data found here is not mine, but is provided by
-            <a href='https://rawg.io/' target='_blank' rel='noreferrer'> RAWG</a>
+            <FontAwesomeIcon
+              icon={faExclamationCircle}
+              className={styles.exclamation}
+              fixedWidth
+            />
+            The content of this website is completely free of charge. The data
+            found here is not mine, but is provided by
+            <a href="https://rawg.io/" target="_blank" rel="noreferrer">
+              {" "}
+              RAWG
+            </a>
           </small>
         </div>
         <div className={styles.container}>
-          <h1 className={styles.page_title}>
-            GameIst Base
-          </h1>
-          <p className={styles.slogan}>Video Game database and game discovery web page.</p>
-          <Link href='/dashboard/1'><a className={styles.button}>Discover Games!</a></Link>
+          <h1 className={styles.page_title}>GameIst Base</h1>
+          <p className={styles.slogan}>
+            Video Game database and game discovery web page.
+          </p>
+          <Link href="/dashboard/1">
+            <a className={styles.button}>Discover Games!</a>
+          </Link>
         </div>
         <div className={`${styles.container} ${styles.rawg_container}`}>
-          <h1 className={`${styles.title} ${styles.rawg_title}`}>
-            RAWG
-          </h1>
+          <h1 className={`${styles.title} ${styles.rawg_title}`}>RAWG</h1>
           <p className={styles.slogan}>
-            All data found here is provided by RAWG.
-            RAWG is the largest video game database and game discovery service.
-            They share 500,000+ games, search, and machine learning recommendations with the world.
+            All data found here is provided by RAWG. RAWG is the largest video
+            game database and game discovery service. They share 500,000+ games,
+            search, and machine learning recommendations with the world.
           </p>
-          <a href='https://rawg.io/apidocs' className={styles.button} target='_blank' rel="noreferrer">About RAWG.!</a>
+          <a
+            href="https://rawg.io/apidocs"
+            className={styles.button}
+            target="_blank"
+            rel="noreferrer"
+          >
+            About RAWG.!
+          </a>
         </div>
-        <div className={styles.container}>
-          <h1 className={`${styles.title} ${styles.footer}`}>
-            © All rights reserved.
-          </h1>
-        </div>
+        <footer className={styles.footer}>
+          <div className={styles.footer_container}>
+            <div className={styles.footer_row}>
+              <div className={styles.footer_col}>
+                <h4>Redes y Contacto</h4>
+                <div className={styles.social_links}>
+                  <p> (569) 62141439</p>
+                </div>
+              </div>
+              <div className={styles.footer_col}>
+                <p>
+                  © All rights reserved.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     </>
   );
